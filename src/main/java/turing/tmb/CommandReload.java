4d3900724386f109140281dcd5f62023746fed8f
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2025-02-11 07:43:04 +0100

    Updated to BTA 7.3_01!

--- src/main/java/turing/tmb/CommandReload.java
+++ src/main/java/turing/tmb/CommandReload.java
@@ -1,15 +1,14 @@
 package turing.tmb;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.ArgumentBuilderLiteral;
 import net.minecraft.core.net.command.CommandManager;
 import net.minecraft.core.net.command.CommandSource;
 
 public class CommandReload implements CommandManager.CommandRegistry {
 	@Override
-	@SuppressWarnings("unchecked")
 	public void register(CommandDispatcher<CommandSource> commandDispatcher) {
-		commandDispatcher.register((LiteralArgumentBuilder) LiteralArgumentBuilder.literal("reload").executes((c) -> {
+		commandDispatcher.register(ArgumentBuilderLiteral.<CommandSource>literal("reload").executes((c) -> {
 			TMB.reloadTMB();
 			return 1;
 		}));
